6443e82fd3be02194223f217d9866ace2680412f
sunish <jsunish@gmail.com>
2014-06-26 04:03:07 +0100

    today mornings commit 26 June 2014

--- src/com/test/socialnw/SocialNWStarter.java
+++ src/com/test/socialnw/SocialNWStarter.java
@@ -1,8 +1,30 @@
 package com.test.socialnw;
 
+import java.io.Console;
+
 public class SocialNWStarter {
-	public static void main(String[] args) {
-		System.out.println("Starting the console based social networking site...");
-	}
+	
+	private static String quit="";
+	
+	  public static final void main(String... aArgs){
+		  while(!quit.equalsIgnoreCase("q")) {
+		    Console console = System.console();
+		    String message = console.readLine("");
+		    quit=message.trim();
+		    
+		    String [] contents = message.split("\\s+");
+		    if(contents.length==1) {
+		    	System.out.println("reading");
+		    }else if(contents[1].equals("->")) {
+		    	System.out.println("posting");
+		    } else if(contents[1].equals("follows")) {
+		    	System.out.println("following");
+		    } else if(contents[1].equals("wall")) {
+		    	System.out.println("wall");
+		    }
+		    
+		  }
+	  }
+		  
 
 }
