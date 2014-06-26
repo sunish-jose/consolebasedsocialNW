f1745e44d80081379b505ebf03a8619daba41926
sunish <jsunish@gmail.com>
2014-06-26 01:32:07 +0100

    Signed-off-by: sunish <jsunish@gmail.com>

--- src/com/test/socialnw/SocialNWStarter.java
+++ src/com/test/socialnw/SocialNWStarter.java
@@ -3,6 +3,7 @@ package com.test.socialnw;
 public class SocialNWStarter {
 	public static void main(String[] args) {
 		System.out.println("Starting the console based social networking site...");
+		System.out.println("Git");
 	}
 
 }
