7ac45592351a1e5cec79ac8084ff68828c9b02e1
sunish <jsunish@gmail.com>
2014-06-26 01:16:30 +0100

    First commit project : console based social networking

---
+++ src/com/test/socialnw/SocialNWStarter.java
@@ -0,0 +1,8 @@
+package com.test.socialnw;
+
+public class SocialNWStarter {
+	public static void main(String[] args) {
+		System.out.println("Starting the console based social networking site...");
+	}
+
+}
