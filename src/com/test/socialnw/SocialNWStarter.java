b9eccb8e05614dc6680942cd309f25b5b71f646c
sunish <jsunish@gmail.com>
2014-06-26 02:28:15 +0100

    test commit

--- src/com/test/socialnw/SocialNWStarter.java
+++ src/com/test/socialnw/SocialNWStarter.java
@@ -3,7 +3,6 @@ package com.test.socialnw;
 public class SocialNWStarter {
 	public static void main(String[] args) {
 		System.out.println("Starting the console based social networking site...");
-		System.out.println("Git");
 	}
 
 }
